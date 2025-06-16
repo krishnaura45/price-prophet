083d018855ca68e1c8726ad23e34304d3d33ff91
Krishna Dubey <kddsai2125@gmail.com>
2025-06-16 23:59:03 +0530

    Added pipelines

---
+++ pipelines/deployment_pipeline.py
@@ -0,0 +1,25 @@
+import os
+from pipelines.training_pipeline import ml_pipeline
+from steps.dynamic_importer import dynamic_importer
+from steps.predictor import predictor
+from zenml import pipeline
+from zenml.integrations.mlflow.steps import mlflow_model_deployer_step
+
+requirements_file = os.path.join(os.path.dirname(__file__), "requirements.txt")
+
+
+@pipeline
+def continuous_deployment_pipeline():
+    """Run a training job and deploy an MLflow model deployment."""
+    # Run the training pipeline
+    trained_model = ml_pipeline()  # No need for is_promoted return value anymore
+
+
+@pipeline(enable_cache=False)
+def inference_pipeline():
+    """Run a batch inference job with data loaded from an API."""
+    # Load batch data for inference
+    batch_data = dynamic_importer()
+
+    # Run predictions on the batch data
+    predictor(service=None, input_data=batch_data)
